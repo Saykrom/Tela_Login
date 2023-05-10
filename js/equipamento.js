e2f464dd2b0430999c7247c99a41b3dc1f894394
Ramon <118860478+Saykrom@users.noreply.github.com>
2023-05-10 19:53:49 -0300

    Delete js directory

--- js/equipamento.js
+++
@@ -1,6 +0,0 @@
-function callApi() {
-    // window.alert('oi');
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-        .then(response => console.log(response))
-        // .then(json => console.log(json))
-}
